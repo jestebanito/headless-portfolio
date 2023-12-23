ab1374f0e779770ca523721ddb59497e371b941f
jestebanito <137816026+jestebanito@users.noreply.github.com>
2023-12-23 09:50:59 -0800

    handled touch events and click events separately for when users are playing on the drum

--- src/components/Drumkeys.jsx
+++ src/components/Drumkeys.jsx
@@ -1,21 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import unmute from "../assets/soundon.svg";
 import mute from "../assets/soundoff.svg";
 
 function Drumkeys(props) {
   const [playing, setPlaying] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
+  const touchHandled = useRef(false);
 
-  const play = () => {
+  const play = (e) => {
+    // Check if the event is from touch or click
+    const isTouchEvent = e && e.type === "touchstart";
+  
     if (!isMuted) {
-      setPlaying(true);
-
       const audio = new Audio(props.sound);
-      audio.play();
-
-      audio.onended = () => {
-        setPlaying(false);
-      };
+  
+      // If it's not playing, play the sound
+      if (!playing || !isTouchEvent) {
+        setPlaying(true);
+        audio.play();
+  
+        audio.onended = () => {
+          setPlaying(false);
+        };
+      } else {
+        // If it's already playing and the event is from touch, restart the sound
+        if (isTouchEvent && !touchHandled.current) {
+          audio.currentTime = 0;
+          audio.play();
+        }
+      }
+    }
+  
+    // Prevent default only for touch events if needed
+    if (isTouchEvent && e.cancelable) {
+      e.preventDefault();
     }
   };
 
@@ -52,8 +70,15 @@ function Drumkeys(props) {
     e.stopPropagation();
   };
 
+  const handleTouchEnd = (event) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
+    touchHandled.current = false;
+  };
+
   return (
-    <div className={`drum ${playing}`} onClick={play}>
+    <div className={`drum ${playing}`} onClick={play} onTouchStart={play} onTouchEnd={handleTouchEnd}>
       <div id={`drum-icons ${props.name}`} className="drum-icons">{props.icon}</div>
       <div className="key-icons">{props.keyIcon}</div>
       <div className="speaker-audio" onClick={toggleMute}>
