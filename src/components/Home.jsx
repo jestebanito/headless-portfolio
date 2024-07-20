cf7c2a5ed1dd006f5500455f49eb4579bd5b6edf
jestebanito <137816026+jestebanito@users.noreply.github.com>
2024-07-20 14:04:57 -0700

    added new client project (zenwellness)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -112,10 +112,10 @@ const Home = ({ restBase, featuredImage }) => {
                             </video>
                             </Link>
                             <div className="react-drum-kit" dangerouslySetInnerHTML={{__html:post.acf.react_drum_kit_overview}}></div>
-                            <div  dangerouslySetInnerHTML={{__html:post.acf.fitness_website_overview}}></div>
                             <div  dangerouslySetInnerHTML={{__html:post.acf.javascript_game_overview}}></div>
                             <div  dangerouslySetInnerHTML={{__html:post.acf.movie_database_overview}}></div>
                             <div  dangerouslySetInnerHTML={{__html:post.acf.portfolio_overview}}></div>
+                            <div  dangerouslySetInnerHTML={{__html:post.acf.zen_wellness_overview}}></div>
                             <Link to={`/work/${post.slug}`}><div dangerouslySetInnerHTML={{__html:post.acf.single_page_button}}></div></Link>
                         </article>
                         )}
